96c9ec1a1126f71df894bb559b05343b43b2d516
turecross321 <turesod@gmail.com>
2023-05-26 00:15:31 +0200

    Fixed environments so they actually work (maybe)

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 import { IEnvironment } from './environment-interface';
 
 export const environment: IEnvironment = {
-  production: false,
   apiBaseUrl: 'http://localhost:10061/api/v1/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 import { IEnvironment } from './environment-interface';
 
 export const environment: IEnvironment = {
-  production: true,
   apiBaseUrl:
     window.location.protocol + '//' + window.location.host + '/api/v1/',
 };
