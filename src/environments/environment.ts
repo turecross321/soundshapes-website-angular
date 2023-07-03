249d0617583e034642f6f10011d2c5f06a84265a
turecross321 <turesod@gmail.com>
2023-07-04 01:36:33 +0200

    WHOOPS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ import { IEnvironment } from './environment-interface';
 export const environment: IEnvironment = {
   production: true,
   apiBaseUrl:
-    window.location.protocol + '//' + window.location.host + '/types/v1/',
+    window.location.protocol + '//' + window.location.host + '/api/v1/',
 };
